f54b0201e679a19f60df4ca6b60fe73a4e7fe895
Valentina Ponamareva <valiazin@gmail.com>
2020-10-22 15:00:31 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  numbersArr = String(n).split('');
+
+  if (numbersArr[0] === '-') {
+    numbersArr.shift();
+  }
+  let reversedNum = Number(numbersArr.reverse().join(''));
+  return reversedNum;
 }
